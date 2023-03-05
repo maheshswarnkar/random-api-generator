2c84a635d4a918dadb02522513894b0d4f6cc3b0
Mahesh <mahesh.swarnkar1984@gmail.com>
2023-03-05 19:45:23 +0530

    Change Controller to RestController

--- src/main/java/com/ms/randomdata/api/RandomApiController.java
+++ src/main/java/com/ms/randomdata/api/RandomApiController.java
@@ -6,11 +6,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.ms.randomdata.api.models.Root;
 import com.ms.randomdata.api.services.RootService;
 
-@Controller
+@RestController
 @RequestMapping("api")
 public class RandomApiController {
 
@@ -18,19 +19,15 @@ public class RandomApiController {
 	RootService rootService;
 
 	@GetMapping("/save")
-	public String saveRandomApi() {
+	public String saveRandomApiData() throws MalformedURLException {
 
-		try {
-			for (int i = 0; i < 100; i++) {
-				Root rootObj = rootService.callRandomApi();
-				rootService.save(rootObj);
+		for (int i = 0; i < 100; i++) {
+			Root rootObj = rootService.callRandomApi();
+			rootService.save(rootObj);
 
-				System.out.println("Record is saved -- " + i + "th");
-			}
-
-		} catch (MalformedURLException e) {
-			e.printStackTrace();
+			System.out.println("Record is saved -- " + i + "th");
 		}
+
 		return "Record is saved !!";
 	}
 
